ba90f87568c450f628dc8be8718f9abc93d67b74
b4ich3v <yoanbaychev@gmail.com>
2026-03-20 22:43:50 +0200

    Implemented new discrete distribution (Hyper geometric distr)

---
+++ source/distributions/discrete_distributions/hyper_geometric/HyperGeometric.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include "source/distributions/discrete_distributions/hyper_geometric/HyperGeometric.h"
+#include "source/data_structures/combinatorics/k_selection/KSelection.h"
+#include "source/data_structures/combinatorics/k_selection/KSelectionPattern.h"
+
+HyperGeometric::HyperGeometric(uint32_t countOfAllObjects, uint32_t countOfMarkedObjects, uint32_t drawnObjects):
+    DiscreteRandomVariable(DiscreteRandomVariableType::HyperGeometric) {
+    if (countOfAllObjects >= countOfMarkedObjects && countOfAllObjects >= drawnObjects) {
+        this->setCountOfAllObjects(countOfAllObjects);
+        this->setCountOfMarkedObjects(countOfMarkedObjects);
+        this->setDrawnObjects(drawnObjects);
+    } else {
+        throw std::runtime_error("Count of all objects must be >= count of marked objects and >= drawn objects");
+    }
+}
+
+double HyperGeometric::calculateProbability(const uint32_t& number) const {
+    uint32_t maxK = std::min(this->drawnObjects, this->countOfMarkedObjects);
+    uint32_t minK = (this->drawnObjects + this->countOfMarkedObjects > this->countOfAllObjects)
+        ? (this->drawnObjects + this->countOfMarkedObjects - this->countOfAllObjects) : 0;
+    if (number < minK || number > maxK) return 0.0;
+
+    double numerator = KSelection(KSelectionPattern::Combination, this->countOfMarkedObjects, number).generateCount() *
+        KSelection(KSelectionPattern::Combination, this->countOfAllObjects - this->countOfMarkedObjects, this->drawnObjects - number).generateCount();
+    double denominator = KSelection(KSelectionPattern::Combination, this->countOfAllObjects, this->drawnObjects).generateCount();
+
+    return numerator / denominator;
+}
+
+double HyperGeometric::getExpectation() const {
+    return (double)(this->drawnObjects * this->countOfMarkedObjects) / this->countOfAllObjects;
+}
+
+double HyperGeometric::getVariance() const {
+    if (this->countOfAllObjects <= 1) return 0.0;
+    return this->getExpectation() * ((double)(this->countOfAllObjects - this->countOfMarkedObjects) / this->countOfAllObjects) *
+        ((double)(this->countOfAllObjects - this->drawnObjects) / (this->countOfAllObjects - 1));
+}
+
+DiscreteRandomVariable<uint32_t>* HyperGeometric::clone() const {
+    return new HyperGeometric(*this);
+}
+
+void HyperGeometric::setCountOfAllObjects(uint32_t countOfAllObjects) {
+    this->countOfAllObjects = countOfAllObjects;
+}
+
+void HyperGeometric::setCountOfMarkedObjects(uint32_t countOfMarkedObjects) {
+    this->countOfMarkedObjects = countOfMarkedObjects;
+}
+
+void HyperGeometric::setDrawnObjects(uint32_t drawnObjects) {
+    this->drawnObjects = drawnObjects;
+}
---
+++ source/distributions/discrete_distributions/hyper_geometric/HyperGeometric.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+#include "source/distributions/discrete_distributions/DiscreteRandomVariable.hpp"
+
+
+class HyperGeometric: public DiscreteRandomVariable<uint32_t> {
+private:
+    uint32_t countOfAllObjects = 0;
+    uint32_t countOfMarkedObjects = 0;
+    uint32_t drawnObjects = 0;
+
+    void setCountOfAllObjects(uint32_t countOfAllObjects);
+    void setCountOfMarkedObjects(uint32_t countOfMarkedObjects);
+    void setDrawnObjects(uint32_t drawnObjects);
+
+public:
+    HyperGeometric(uint32_t countOfAllObjects, uint32_t countOfMarkedObjects, uint32_t drawnObjects);
+
+    double calculateProbability(const uint32_t& number) const override;
+    double getExpectation() const override;
+    double getVariance() const override;
+    DiscreteRandomVariable<uint32_t>* clone() const override;
+};
